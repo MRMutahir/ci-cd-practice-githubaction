284cec6d9c1deb2c68437fd91690adb14a07fd8e
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-12-24 17:57:25 +0500

    create / user API

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ const port = 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
-// Example route for GET
+// Dummy data for users
+const users = [
+    { id: 1, name: 'Ali', age: 25 },
+    { id: 2, name: 'Abbas', age: 30 },
+    { id: 3, name: 'Ahmed', age: 22 },
+    { id: 4, name: 'Anus', age: 28 }
+];
+
+// Example route for GET / 
 app.get('/', (req, res) => {
     res.send('SALAM World!');
+});
+
+// GET /user API
+app.get('/user', (req, res) => {
+    res.json(users);
 });
 
 // Start server
