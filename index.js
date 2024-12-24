1d2e8d483b20f064bc0f0fb3dd29156e778e0ec0
Muhammad Mutahir <mutahirkareem820@gmail.com>
2024-12-24 18:04:05 +0500

    remove unnecessary commit

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON
+
 app.use(express.json());
 
-// Dummy data for users
 const users = [
     { id: 1, name: 'Ali', age: 25 },
     { id: 2, name: 'Abbas', age: 30 },
@@ -13,17 +12,16 @@ const users = [
     { id: 4, name: 'Anus', age: 28 }
 ];
 
-// Example route for GET / 
 app.get('/', (req, res) => {
     res.send('SALAM World!');
 });
 
-// GET /user API
+
 app.get('/user', (req, res) => {
     res.json(users);
 });
 
-// Start server
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
